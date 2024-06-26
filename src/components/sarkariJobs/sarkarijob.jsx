59f89bc029e6cc73a22d268fade6adcb048251c1
sdeabhi <abhinandan.prajapati@kindasolutions.com>
2024-06-26 15:45:28 +0530

    update

--- src/components/sarkariJobs/sarkarijob.jsx
+++ src/components/sarkariJobs/sarkarijob.jsx
@@ -7,47 +7,68 @@ const SarkariJob = () => {
     setIsExpanded(!isExpanded);
   };
   return <>
- <Accordion type="single" collapsible className="w-full hover:bg-gray-50 rounded-lg px-5">
-      <AccordionItem value="item-1">
-        <AccordionTrigger onClick={handleToggle}>
-          <div className="flex justify-between items-center w-full">
-            <div className="flex justify-between items-center gap-5">
-              <img src={sarkari} alt="Sarkari" />
-              <h2 className="text-xl font-bold flex-grow">Rajasthan Board Class 10th Result 2024</h2>
-            </div>
-           
-            {!isExpanded && (
-               <div>
-              <button className="bg-sarkaribg text-lg text-sarkari px-4 py-2 hover:bg-white rounded-md">
-                Apply Job
-              </button>
-              </div>
-            )}
-          </div>
-        </AccordionTrigger>
-        <AccordionContent>
-          <div className="w-full flex justify-around">
-            <div className="gap-5">
-              <p className="flex gap-3">Application Begin <li className="text-gray-500">29/05/2024</li></p>
-              <p className="flex gap-3">Last Date for Apply <li className="text-gray-500">29/05/2024</li></p>
-              <p className="flex gap-3">Exam / Merit List <li className="text-gray-500">As per Schedule</li></p>
-              <p className="flex gap-3">Age Eligibility <li className="text-gray-500">18 - 32 Years</li></p>
-            </div>
-            <div className="w-250 text-sarkari h-5"></div>
-            <div className="gap-5">
-              <p className="flex gap-3">General / OBC / EWS Fees <li className="text-gray-500"> ₹0/-</li></p>
-              <p className="flex gap-3">SC / ST / PH <li className="text-gray-500"> ₹0/-</li></p>
-              <p className="flex gap-3">Education <li className="text-gray-500"> 10th pass</li></p>
-            </div>
-          </div>
-          <div className="w-full flex justify-center items-center mt-10">
-            <button className="bg-sarkaribg text-lg text-sarkari px-4 py-2 hover:bg-[#05CFC3] hover:text-white rounded-md">
-              Apply Job
+<Accordion type="single" collapsible className="w-full hover:bg-gray-50 rounded-lg px-5">
+  <AccordionItem value="item-1">
+    <AccordionTrigger onClick={handleToggle}>
+      <div className="flex justify-between items-center w-full">
+        <div className="flex justify-between items-center gap-5">
+          <img src={sarkari} alt="Sarkari" className="" />
+          <h2 className="text-lg font-bold flex-grow">Rajasthan Board Class 10th Result 2024</h2>
+        </div>
+        {!isExpanded && (
+          <div>
+            <button className="bg-sarkaribg text-lg text-sarkari px-4 py-2 hover:bg-white rounded-md">
+              View Job
             </button>
           </div>
-        </AccordionContent>
-      </AccordionItem>
-    </Accordion>
+        )}
+      </div>
+    </AccordionTrigger>
+    <AccordionContent>
+      <div className="w-full flex-col justify-around">
+        <div className="gap-5">
+          <div className="flex gap-3 items-center">
+            <p>Application Begin</p>
+            <li className="text-gray-500 ml-auto">29/05/2024</li>
+          </div>
+          <div className="flex gap-3 items-center">
+            <p>Last Date for Apply</p>
+            <li className="text-gray-500 ml-auto">29/05/2024</li>
+          </div>
+          <div className="flex gap-3 items-center">
+            <p>Exam / Merit List</p>
+            <li className="text-gray-500 ml-auto">As per Schedule</li>
+          </div>
+          <div className="flex gap-3 items-center">
+            <p>Age Eligibility</p>
+            <li className="text-gray-500 ml-auto">18 - 32 Years</li>
+          </div>
+        </div>
+        <div className="border-r-2 sm:border-b-2 border-gray-300"></div>
+        <div className="gap-5">
+          <div className="flex gap-3 items-center">
+            <p>General / OBC / EWS Fees</p>
+            <li className="text-gray-500 ml-auto">₹0/-</li>
+          </div>
+          <div className="flex gap-3 items-center">
+            <p>SC / ST / PH</p>
+            <li className="text-gray-500 ml-auto">₹0/-</li>
+          </div>
+          <div className="flex gap-3 items-center">
+            <p>Education</p>
+            <li className="text-gray-500 ml-auto">10th pass</li>
+          </div>
+        </div>
+      </div>
+      <div className="w-full flex justify-center items-center mt-10">
+        <button className="bg-sarkaribg text-lg text-sarkari px-4 py-2 hover:bg-[#05CFC3] hover:text-white rounded-md">
+          Apply Job
+        </button>
+      </div>
+    </AccordionContent>
+  </AccordionItem>
+</Accordion>
+
   </>;
 };
 
--- src/pages/LatestNews.jsx
+++ src/pages/LatestNews.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import NewsList from "../components/latestNews/NewsList";
+import newsAds from "../assets/newsAds.gif";
 
 const LatestNews = () => {
-  return <div>LatestNews</div>;
+  return <>
+   
+  <div className="my-8">
+  <img src={newsAds} alt="news page ads" />
+      <h1 className='text-lg font-semibold text-primary'>Trending Jobs News</h1>
+  </div>
+
+  
+
+  <div className='w-full flex gap-5'>
+ {/* data portion */}     
+  <div className='lg:w-3/4'>
+  
+   <NewsList/>
+   <NewsList/>
+   <NewsList/>
+   <NewsList/>
+  </div>
+
+
+
+
+
+
+  {/* advertisement partion.. hidden on small screen */}
+  <div className='hidden lg:flex border-2 w-1/4'>
+     {/* <img src={iconpvt} alt="" /> */}
+     <div className="w-full gap-10">
+     <div className="border-2 w-full mb-2" style={{ height: "200px"}}></div>
+     <div className="border-2 w-full mb-2" style={{ height: "200px"}}></div>
+     <div className="border-2 w-full mb-2" style={{ height: "200px"}}></div>
+  </div>
+  </div>
+
+  </div>
+  </>;
 };
 
 export default LatestNews;
